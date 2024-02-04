3efa1cd7cefdda17daf436430c8f7462425b7545
Ali <alkhdyyr@gmail.com>
2024-02-04 14:03:53 +0100

    link some website with live

---
+++ main.js
@@ -0,0 +1,4 @@
+let project = document.querySelectorAll(".project-card");
+let projectsCounter = document.querySelector(".projects-counter");
+let numberCounter = document.querySelector(".number-count");
+numberCounter.innerHTML = project.length + "+";
